15b3fde018d66b92dc4d9a50d6a3bd8874617a64
Fredx05x <f.gaudreau05@gmail.com>
2020-03-01 12:57:09 -0500

    modify eslint rules

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const app = express()
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+
+const app = express()
 
 // LOAD ENV VARS
 dotenv.config({ path: './config/config.env' })
@@ -11,11 +12,11 @@ app.use(bodyParser.json())
 
 // MAIN ROUTES
 app.get('/', (req, res) => {
-    res.send("La Maison d'Aurore API")
+  res.send("La Maison d'Aurore API")
 })
 
 // CONNECT TO SERVER
 const PORT = process.env.PORT || 8080
-app.listen(PORT, function () {
-    console.log('Server listening on: http://localhost:%s', PORT)
+app.listen(PORT, () => {
+  console.log('Server listening on: http://localhost:%s', PORT)
 })
