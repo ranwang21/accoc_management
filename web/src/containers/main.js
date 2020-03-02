f04a01b72ad6d0b4735e6cc9a8067e4b57c9a23d
Derrick <dtoihoun@isi-mtl.com>
2020-03-02 08:32:26 -0500

    uses case gestions admin

---
+++ web/src/containers/main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Main extends Component {
+    constructor () {
+        super()
+        this.state = {}
+    }
+
+    componentDidMount () {}
+
+    render () {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+export default Main
---
+++ web/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Main from './containers/main'
+
+ReactDOM.render(
+    <Main />,
+    document.getElementById('app')
+)
