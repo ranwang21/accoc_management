9c8d831cd616e93b21f3dd2b16a490b847ad0a05
djemaniyannick <yanger842@gmail.com>
2020-03-06 11:14:20 -0500

    structure du projet android

---
+++ AndroidMaisonDaurore/app/src/main/java/com/example/androidmaisondaurore/controllers/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.androidmaisondaurore.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidmaisondaurore.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
