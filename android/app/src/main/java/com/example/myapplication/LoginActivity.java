00e34be681d047175170c9b5791d0e682fe95227
djemaniyannick <yanger842@gmail.com>
2020-03-09 17:40:29 -0400

    menu drawer done

--- android/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ android/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,16 +1,15 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_activity);
     }
 }
