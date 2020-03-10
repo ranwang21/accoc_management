819523a9668e494d58f6c6b2915c4a2a039f6cbc
ran wang <wang.ran.henri@gmail.com>
2020-03-10 11:48:37 -0400

    fix table menu

--- web/src/components/Table.js
+++ web/src/components/Table.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import TableListMenu from './table-list-menu'
-import '../styles/_footer.scss'
+import '../styles/_table.scss'
 
 class Table extends Component {
     getLangFile () {
