abc8a68300821aeae9e48f69ae20a85569506f17
jjilani <jihene.jilani@gmail.com>
2020-03-12 10:20:03 -0400

    adapter presence acceuil fix

---
+++ android/app/src/main/java/com/example/myapplication/adapters/PresenceAdapter.java
@@ -0,0 +1,37 @@
+package com.example.myapplication.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.myapplication.R;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+
+public class PresenceAdapter extends ArrayAdapter{
+
+Context context;
+List<String>modelItems;
+    public PresenceAdapter(Context context, List<String> resource) {
+        super(context, R.layout.fragment_presence ,resource);
+        this.context=context;
+this.modelItems=resource;
+    }
+    public View getView(int position, View convertView, ViewGroup parent){
+        LayoutInflater inflater =(( Activity )context).getLayoutInflater();
+        convertView=inflater.inflate(R.layout.fragment_presence_row,parent,false);
+        TextView name =(TextView) convertView.findViewById(R.id.textView1);
+        CheckBox cb =(CheckBox)convertView.findViewById(R.id.checkBox1);
+        ImageView img=(ImageView )convertView.findViewById(R.id.imageView3);
+        name.setText(modelItems.get(position));
+        return convertView;
+    }
+}
