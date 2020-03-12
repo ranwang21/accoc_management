0e0736985ff0956bba92e96449db0cdd033880f3
djemaniyannick <yanger842@gmail.com>
2020-03-12 10:34:08 -0400

    vue suivi quotidien done

--- android/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ android/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,14 +1,10 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import com.example.myapplication.entities.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
