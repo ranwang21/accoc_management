1c50f0a86f77e0b210e12c1eaf00a27b5a0e523c
Fredx05x <f.gaudreau05@gmail.com>
2020-03-16 16:09:34 -0400

    Add Send Email with Reset Password functionality

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const { protect } = require('../middlewares/auth')
-const { login, getUser } = require('../controllers/auth')
+const {
+  login,
+  getUser,
+  forgotPassword,
+  resetPassword
+} = require('../controllers/auth')
 
 const router = express.Router()
 
 router.post('/login', login)
+router.post('/forgot-password', forgotPassword)
 router.get(
   '/user',
   // protect,
   getUser
 )
+router.put('/reset-password/:token', resetPassword)
 
 module.exports = router
