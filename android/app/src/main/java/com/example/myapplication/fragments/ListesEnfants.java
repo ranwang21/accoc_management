ac563ae9a98842fd2a3c480d63bf1bb7bd05bf27
yannick djemani <yanger842@gmail.com>
2020-03-27 13:40:51 -0400

    page suivi quotidien

--- android/app/src/main/java/com/example/myapplication/fragments/ListesEnfants.java
+++ android/app/src/main/java/com/example/myapplication/fragments/ListesEnfants.java
@@ -1,13 +1,16 @@
 package com.example.myapplication.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.SuiviQuotidienActivity;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -15,24 +18,29 @@ import androidx.fragment.app.Fragment;
 
 public class ListesEnfants extends Fragment {
 
+    ListView listView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
         View view = inflater.inflate(R.layout.fragment_listes_enfants, container, false);
-
-
         String[] listviewItems = {"first thing", "second thing", "third thing", "forth thing", "fifth thing"};
-        ListView listView = view.findViewById(R.id.list_enfant);
+        listView = view.findViewById(R.id.list_enfant);
         ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
                 getActivity(),
                 android.R.layout.simple_list_item_activated_1, listviewItems
         );
         listView.setAdapter(listViewAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), SuiviQuotidienActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
-
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
