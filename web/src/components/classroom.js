fe027fb8bc63ebeecfb0ddba506552d8dda19643
ran wang <wang.ran.henri@gmail.com>
2020-04-07 11:22:06 -0400

    unfinished classroom

--- web/src/components/classroom.js
+++ web/src/components/classroom.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-import List from '@material-ui/core/List'
-import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
 import '../styles/_classroom.scss'
 
@@ -15,33 +12,24 @@ class ClassRoom extends Component {
         const lang = this.getLangFile()
 
         return (
-            <div className='classroom'>
-                <div className='classroom-title'>
-                    {/* TODO: dynamically change classroom number with props */}
-                    <h3>{lang.classRoomName}</h3>
-                </div>
-                <List component='nav' aria-label='main mailbox folders'>
-                    <ListItem button>
-                        <ListItemText primary='Mr. Bean' />
-                    </ListItem>
-                    <ListItem button>
-                        <ListItemText primary='Ms. Brown' />
-                    </ListItem>
-                </List>
-                <div>
-                    <h3>{lang.presented}</h3>
-                    {/* TODO */}
-                    <p className='child-number'>6</p>
-                </div>
-                <div>
-                    <h3>{lang.absent}</h3>
-                    {/* TODO */}
-                    <p className='child-number'>10</p>
-                </div>
-                <Button variant='outlined' color='primary'>
-                    {lang.seeDetail}
-                </Button>
-            </div>
+            <form noValidate>
+                <TextField
+                    id='date-start'
+                    label={lang.from}
+                    type='date'
+                    InputLabelProps={{
+                        shrink: true
+                    }}
+                />
+                <TextField
+                    id='date-end'
+                    label={lang.to}
+                    type='date'
+                    InputLabelProps={{
+                        shrink: true
+                    }}
+                />
+            </form>
         )
     }
 }
