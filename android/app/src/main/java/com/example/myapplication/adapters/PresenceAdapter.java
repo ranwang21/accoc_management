58a4b310fb0ebaa23b427ee59f7bf988491f38a6
jjilani <jihene.jilani@gmail.com>
2020-04-09 12:35:23 -0400

    chekbox

--- android/app/src/main/java/com/example/myapplication/adapters/PresenceAdapter.java
+++ android/app/src/main/java/com/example/myapplication/adapters/PresenceAdapter.java
@@ -2,6 +2,7 @@ package com.example.myapplication.adapters;
 
 import android.app.Activity;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,16 +11,19 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.myapplication.R;
 import com.example.myapplication.entities.Schedule;
 import com.example.myapplication.entities.User;
+import com.example.myapplication.services.ConnectionBD;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PresenceAdapter extends ArrayAdapter<User> {
-
+    private ArrayList<Schedule> schedList;
    // Context context;
     //List<String> modelItems;
 int idLayout;
@@ -29,25 +33,47 @@ int idLayout;
         idLayout=resource;
         //     this.context = context;
      //   this.modelItems = resource;
+    }private class ViewHolder{
+        CheckBox cb;
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
         final User user =getItem(position);
-
+ViewHolder holder=null;
         if(convertView==null) {
             convertView=LayoutInflater.from(getContext()).inflate(idLayout,null);
             //LayoutInflater inflater = (( Activity ) context).getLayoutInflater();
           //  convertView = inflater.inflate(R.layout.fragment_presence_row, parent, false);
+            holder=new ViewHolder();
             Button btn=convertView.findViewById(R.id.button_presence);
             TextView Fname = convertView.findViewById(R.id.textView1);
             TextView Lname = convertView.findViewById(R.id.textView2);
-
+holder.cb =convertView.findViewById(R.id.checkBox1);
             ImageView img = convertView.findViewById(R.id.imageView3);
             Fname.setText(user.getFirst_name());
             Lname.setText(user.getLast_name());
             Glide.with(getContext()).load(user.getImg_url()).into(img);
-
+convertView.setTag(holder);
+holder.cb.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        SQLiteDatabase bd = ConnectionBD.getBd(getContext());
+        CheckBox checkBox=(CheckBox) v;
+        Schedule schedule =(Schedule)checkBox.getTag();
+        Toast.makeText(getContext(),"Clicked on checkbox"+checkBox.getText()+"is"+checkBox.isChecked(),Toast.LENGTH_LONG).show();
+        schedule.setIs_absent(checkBox.isChecked());
+    }
+});
+        }else {
+            holder=(ViewHolder)convertView.getTag();
         }
+       /* Schedule schedule=schedList.get(position);
+        holder.cb.setText(schedule.getDate());
+        holder.cb.setChecked(schedule.getIs_absent());
+        holder.cb.setTag(schedule);*/
+
         return convertView;
     }
+
+
 }
--- android/app/src/main/java/com/example/myapplication/fragments/Presence.java
+++ android/app/src/main/java/com/example/myapplication/fragments/Presence.java
@@ -46,21 +46,7 @@ ListView listView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-      /*  //returning our layout file
-        //change R.layout.yourlayoutfilename for each of your fragments
-        View view = inflater.inflate(R.layout.fragment_presence, container, false);
 
-
-        ListView listView = view.findViewById(R.id.list_presence);
-        /*
-        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                android.R.layout.simple_list_item_checked,listviewItems
-        );
-        listView.setAdapter(listViewAdapter);
-        PresenceAdapter adapter = new PresenceAdapter(getActivity(), listviewItems);
-        listView.setAdapter(adapter);*/
-CheckboxAdapter checkboxAdapter;
       PresenceAdapter presenceAdapter;
       ArrayList<User> users=new ArrayList<>();
       ArrayList<Role> roles = RoleManager.getAll(getContext());
@@ -109,45 +95,22 @@ CheckboxAdapter checkboxAdapter;
 
            }
        });
+       CheckBox checkBox =view.findViewById(R.id.checkBox1);
+
+
         ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
                 getContext(),
                 android.R.layout.simple_list_item_checked
         );
+
         listView.setAdapter(listViewAdapter);
-        CheckBox checkBox =view.findViewById(R.id.checkBox1);
-        ArrayList<Schedule>schedules=ScheduleManager.getAll(getContext());
-        ArrayList<String>listCheck=new ArrayList<>();
-        listCheck.add("Tous");
-        for(Schedule s:schedules){
-            listCheck.add(s.get_id());
-        }
-     /*   checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                CheckboxAdapter checkboxAdapter1=new CheckboxAdapter(getContext(),R.id.checkBox1,schedules);
-                listView.setAdapter(checkboxAdapter1);
-                checkboxAdapter1.notifyDataSetChanged();
-            }
-        });
-
-      /*  ArrayList<Schedule> schedules=ScheduleManager.getAll(getContext());
-        for(Schedule sch:schedules){
-            Button btnSave=new Button(getContext());
-            btnSave.findViewById(R.id.button_presence);
-            btnSave.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    DateFormat date =new SimpleDateFormat("MMM dd yyyy, h:mm");
-                    String dateFormat =date.format(Calendar.getInstance().getTime());
-                    Schedule schedule=new Schedule();
-                    schedule.setDate(dateFormat);
-                    ScheduleManager.insert(getContext(),schedule);
-                    //CheckboxAdapter checkboxAdapter =new CheckboxAdapter(getContext(),R.layout.fragment_presence_row,schedule.get_id());
-                    CheckBox checkBox=view.findViewById(R.id.checkBox1);
-                    Toast.makeText(getActivity(), "Button Clicked", Toast.LENGTH_LONG).show();
-                }
-            });*/
+listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Schedule schedule=(Schedule)parent.getItemAtPosition(position);
+        Toast.makeText(getContext(),"Clicked on row"+schedule.get_id(),Toast.LENGTH_LONG).show();
+    }
+});
 
 
 
